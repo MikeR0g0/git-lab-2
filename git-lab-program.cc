571bb22408e7badbad871d63aaa941e9c1f615c0
Michael Rogowski <rogowski478@gmail.com>
2025-01-24 15:04:51 -0500

    Hello Git program functioning as intended

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Michael Joseph Rogowski Jr
+ *        Date: 1/24/2025
+ * Description: Git Lab 2 Assignment
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
